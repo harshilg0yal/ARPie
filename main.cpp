937c5f9783d8ced6f41059e3fa1d8f39ffa0e495
harshilg0yal <harshilgoyal17@gmail.com>
2024-12-12 22:12:02 +0530

    Changed the variable type for interface List and also changed the variable names for more clarity

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 int main(int argc, char* argv[])
 {
     int option=0;
-    auto interfaces=listInterface();
+    auto interfaceList=listInterface();
     std::cout<<"Please enter the Interface No. for which you want to sniff packets: ";
-    std::cin>>option;
+    std::cin>>option; 
     std::cout<<std::endl;
-    auto interface = interfaces[option];
-    CapturePackets(interface);
+    auto userInterface = interfaceList[option];
+    CapturePackets(userInterface);
 }
